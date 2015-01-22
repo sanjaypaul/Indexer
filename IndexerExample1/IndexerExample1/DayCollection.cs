232743e09109dfb58f1350da2b0556def04ba98e
sanjaypaul <sanjaypaul.bd@gmail.com>
2015-01-22 15:36:22 +0600

    fdf

---
+++ IndexerExample1/IndexerExample1/DayCollection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace IndexerExample1
+{
+    class DayCollection
+    {
+        string[] days = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+
+        public int this[string aDay]
+        {
+            get
+            {
+                for (int index = 0; index < days.Length; index++)
+                {
+                    if (days[index] == aDay)
+                    {
+                        return index;
+                    }
+                }
+
+                throw new ArgumentOutOfRangeException(aDay, "aDay must be in the form \"Sun\", \"Mon\", etc");
+            }
+        }
+
+        public string this[int dayIndex]
+        {
+            get
+            {
+                for (int index = 0; index < days.Length; index++)
+                {
+                    if (index == dayIndex)
+                    {
+                        return days[index];
+                    }
+                }
+                throw new ArgumentOutOfRangeException("Day index must be between 0 to 6");
+            }
+        }
+    }
+}
---
+++ IndexerExample1/IndexerExample1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace IndexerExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DayCollection week = new DayCollection();
+            int a = week["Monday"];
+
+            
+
+            Console.WriteLine(week["Friday"]);
+
+            // Raises ArgumentOutOfRangeException
+            //Console.WriteLine(week["Made-up Day"]);
+
+            Console.WriteLine(week[0]);
+
+            Console.ReadKey();
+        }
+    }
+}
